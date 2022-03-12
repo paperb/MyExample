175fdc139c6eb3605e2bf490a07bce8f12f4bdff
paperB <938327915@qq.com>
2022-03-12 22:27:39 +0800

    Initial commit

---
+++ src/main/java/com/gonownowait/myexample/MyExampleApplication.java
@@ -0,0 +1,13 @@
+package com.gonownowait.myexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gonownowait/myexample/MyExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gonownowait.myexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
